28ceaad43a2539bf26bbc40878f7169d6e75b496
dmnbars <dmnbars@gmail.com>
2020-06-25 20:11:19 +0300

    implement only first capitalizer

--- formatter/capitalizer.py
+++ formatter/capitalizer.py
@@ -7,6 +7,14 @@ def simple_capitalize(name):
     return ' '.join([str.capitalize(word) for word in str.split(name, ' ')])
 
 
+def first_letter_to_upper(string):
+    return "%s%s" % (str.upper(string[:1]), string[1:])
+
+
+def only_first_capitalizer(name):
+    return ' '.join([first_letter_to_upper(word) for word in str.split(name, ' ')])
+
+
 class KeywordCapitalizer(ast.NodeVisitor):
     def visit_KeywordName(self, node):
         token = node.get_token(Token.KEYWORD_NAME)
