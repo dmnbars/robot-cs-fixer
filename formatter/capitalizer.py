ddce5f4aa12d596c524887a2ffa669b2acb2b744
dmnbars <dmnbars@gmail.com>
2020-06-25 20:03:14 +0300

    rename capitalizer

--- formatter/capitalizer.py
+++ formatter/capitalizer.py
@@ -3,15 +3,15 @@ import ast
 from robot.api import Token
 
 
-def capitalize(name):
+def simple_capitalize(name):
     return ' '.join([str.capitalize(word) for word in str.split(name, ' ')])
 
 
 class KeywordCapitalizer(ast.NodeVisitor):
     def visit_KeywordName(self, node):
         token = node.get_token(Token.KEYWORD_NAME)
-        token.value = capitalize(token.value)
+        token.value = simple_capitalize(token.value)
 
     def visit_KeywordCall(self, node):
         token = node.get_token(Token.KEYWORD)
-        token.value = capitalize(token.value)
+        token.value = simple_capitalize(token.value)
--- formatter/capitalizer_test.py
+++ formatter/capitalizer_test.py
@@ -2,4 +2,4 @@ import formatter.capitalizer
 
 
 def test_simple_capitalizer():
-    assert "Test Keyword Capitalized Correctly" == formatter.capitalizer.capitalize("test keyWorD CAPITALIZED Correctly")
+    assert "Test Keyword Capitalized Correctly" == formatter.capitalizer.simple_capitalize("test keyWorD CAPITALIZED Correctly")
